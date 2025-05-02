1c7b17186f269a5523e71cfd0284b71269c7cfe6
MacBook <ramez@akera.agency>
2025-05-03 00:10:06 +0800

    Added pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import PHProvider from "@/app/provider";
+import PHProvider from "@/app/providers/posthog";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -26,6 +26,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="rtl">
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
+              n.callMethod.apply(n,arguments):n.queue.push   
+              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
+              0;n.version='2.0';n.queue=[];t=b.createElement(e);
+              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '668538532566082');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=668538532566082&ev=
+            PageView&noscript=1"/>
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={`${sora.style} antialiased bg-background relative`}>
         <PHProvider>
           <NavGlowing />
--- src/app/providers/posthog.tsx
+++ src/app/providers/posthog.tsx
@@ -3,12 +3,17 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
+
+const PixelTracker = dynamic(() => import("./meta"), { ssr: false });
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
 }
+
+
 
 export default function PHProvider({
   children,
@@ -18,8 +23,14 @@ export default function PHProvider({
   const [queryClient] = useState(() => new QueryClient());
 
   return (
+    <div>
+      
     <PostHogProvider client={posthog}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <PixelTracker />
+        {children}
+      </QueryClientProvider>
     </PostHogProvider>
+    </div>
   );
 }
