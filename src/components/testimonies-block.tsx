c820df0b34794ecb8744e2e98feda98863e33fa1
Idriss <idrissgarfa@outlook.com>
2025-08-09 10:02:56 +0100

    fix(testimonies-block): identical rating star keys

--- src/components/testimonies-block.tsx
+++ src/components/testimonies-block.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import RatingStar from "@/components/ui/icons/RatingStar";
-import Video from "@/components/ui/video";
-import { TESTIMONY_CONTENT } from "@/content/testimony";
 import Image from "next/image";
+
+import Video from "@/components/ui/video";
+import RatingStar from "@/components/ui/icons/RatingStar";
+
+import { TESTIMONY_CONTENT } from "@/content/testimony";
+
 const TestimoniesBlock = () => {
   const testimoniesArray = TESTIMONY_CONTENT.testimony.details;
   return (
-    <div className="">
-      {testimoniesArray.map((testimonies , index) => (
+    <div>
+      {testimoniesArray.map((testimonies, index) => (
         <div className="w-full h-screen min-w-[100%]" key={`block-${index}`}>
           <div className="flex flex-col items-end gap-6 lg:py-4 lg:pl-16">
             <div className="flex gap-2">
-              {[...Array(testimonies.rating)].map(() => {
-                return <RatingStar key={`star-${index}`} />;
+              {[...Array(testimonies.rating)].map((_, starIndex) => {
+                return <RatingStar key={`star-${index}-${starIndex}`} />;
               })}
             </div>
             {testimonies.thumbnail && testimonies.videoCode ? (
